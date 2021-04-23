2ed7d6c040e1be0939b03addc3917e21adce6352
breeze <ts001_breeze@space-p.com>
2021-04-23 18:35:25 +0800

    add new salt

--- migrations/5_add_salt.js
+++ migrations/5_add_salt.js
@@ -12,7 +12,7 @@ const getCardInfo = (_network) => {
 
 // add new cards
 // const addCardList = [1, 2, 3, 4, 5, 6, 7, 8]
-const addCardList = [9, 10, 11, 12, 13, 14, 15, 16]
+const addCardList = [17, 18, 19]
 
 // ============ Main Migration ============
 
